60c16d8b4901c164a1a8ae01681fc78392ea4382
Fabio Fortkamp <fabio@fabiofortkamp.com>
2015-04-17 10:13:28 -0300

    Fixed another typo in functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -37,7 +37,7 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assserTrue(
+        self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows)
             )
             
