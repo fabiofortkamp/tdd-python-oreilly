11e9065fba26845b4e24232ea72b77fade1301fc
Fabio Fortkamp <fabio@fabiofortkamp.com>
2015-06-09 11:06:04 -0300

    rename all item input ids and names. still broken

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -49,7 +49,7 @@ class ListViewTest(TestCase):
 
         self.client.post(
             '/lists/new',
-            data={'item_text': 'A new list item'})
+            data={'text': 'A new list item'})
         
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
@@ -59,7 +59,7 @@ class ListViewTest(TestCase):
 
         response=self.client.post(
             '/lists/new',
-            data={'item_text': 'A new list item'})
+            data={'text': 'A new list item'})
 
         new_list = List.objects.first()
 
@@ -77,7 +77,7 @@ class ListViewTest(TestCase):
 
         self.client.post(
             '/lists/%d/' % (correct_list.id,),
-            data={'item_text': 'A new item for an existing list'}
+            data={'text': 'A new item for an existing list'}
             )
 
         self.assertEqual(Item.objects.count(),1)
@@ -90,7 +90,7 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
         response = self.client.post(
             '/lists/%d/' % (correct_list.id,),
-            data={'item_text': 'A new item for an existing list'}
+            data={'text': 'A new item for an existing list'}
             )
 
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
@@ -99,7 +99,7 @@ class ListViewTest(TestCase):
         list_ = List.objects.create()
         response = self.client.post(
             '/lists/%d/' % (list_.id,),
-            data={'item_text': ''}
+            data={'text': ''}
             )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'list.djhtml')
@@ -109,14 +109,14 @@ class ListViewTest(TestCase):
 class NewListTest(TestCase):
 
     def test_validation_errors_are_sent_back_to_home_page_template(self):
-        response = self.client.post('/lists/new', data={'item_text': ''})
+        response = self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.djhtml')
         expected_error = escape("You can't have an empty list item")
         self.assertContains(response, expected_error)
 
     def test_invalid_list_items_arent_saved(self):
-        self.client.post('/lists/new', data={'item_text': ''})
+        self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
